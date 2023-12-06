39d9bd16b2bbbae8bbd62d413b82b4068d0c0407
Nikolett Jávor <nikolettjavor@gmail.com>
2023-12-06 14:45:29 +0100

    23.12.06

---
+++ README.md
@@ -0,0 +1,1 @@
+# Szimulációs projektmunka - fogolydilemma
---
+++ static/js/ember.js
@@ -0,0 +1,17 @@
+class Ember{
+    constructor(nev, poz, habitus){
+        this.nev = nev;
+        this.poz = poz;
+        this.habitus = habitus;
+    }
+
+    // létezés
+    torol(){
+        delete this;
+    }
+
+
+    mozogj(){
+        
+    }
+}
